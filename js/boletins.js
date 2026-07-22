d028559e0fbb5db624fdb455848f7b69bcc54bf9
ruralinai-commits <ruralin.ai@gmail.com>
2026-07-22 18:30:58 -0300

    Update boletins.js

--- js/boletins.js
+++ js/boletins.js
@@ -28,6 +28,13 @@
 window.BOLETINS = [
 
   {
+    arquivo: "info-13-22-jul.html",
+    data: "22 jul. 2026",
+    titulo: "Boletim Semanal de Soja, Milho, Boi e Dólar",
+    categoria: "Boletim"
+  },
+
+  {
     arquivo: "info-mercado-03-jul.html",
     data: "03 jul. 2026",
     titulo: "Boletim Semanal de Soja, Milho, Boi e Dólar",
